5e3c063f05a74188de250794a6705ce206dbda83
Theo Vora <theo.vora@gmail.com>
2024-03-16 20:44:07 -0500

    BROKEN: add StatusEditor

--- MastAutoDonCompletion/ContentView.swift
+++ MastAutoDonCompletion/ContentView.swift
@@ -6,14 +6,19 @@
 //
 
 import SwiftUI
+import StatusKit
+import Models
 
 struct ContentView: View {
+    let account = Account(id: "", username: "", displayName: "", avatar: URL(string: "https://apple.com")!, header: URL(string: "https://apple.com")!, acct: "", note: HTMLString(stringValue: "", parseMarkdown: false), createdAt: ServerDate(), followersCount: 0, followingCount: 0, statusesCount: 0, lastStatusAt: "", fields: [], locked: false, emojis: [], bot: false)
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            StatusEditor.MainView(mode: .mention(account: account, visibility: .pub))
         }
         .padding()
     }
