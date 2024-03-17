f1b962e32e0f4d8055d057b8b740f6527a5f9ded
Theo Vora <theo.vora@gmail.com>
2024-03-16 19:43:46 -0500

    Initial Commit

---
+++ MastAutoDonCompletion/MastAutoDonCompletionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastAutoDonCompletionApp.swift
+//  MastAutoDonCompletion
+//
+//  Created by Tana Vora on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MastAutoDonCompletionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
